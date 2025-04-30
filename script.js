022d0c757578412f54ce943d3de8fe5ab91f4129
Jackson Ingerson <jacksoningerson@gmail.com>
2025-04-30 06:28:28 -0400

    index.html

---
+++ script.js
@@ -0,0 +1,81 @@
+const choices =['rock','paper','scissors']
+
+function getComputerChoice()
+{
+ const choice = choices[Math.floor(Math.random()*choices.length)]
+ return choice; 
+}
+
+function getPlayerChoice()
+{
+  let validatedInput = false;
+  while (validatedInput == false)
+  {
+    const choice = prompt('rock,paper,scissors');
+    if (choice == null) {continue};
+    const choiceInLower = choice.toLowerCase();
+    if(choices.includes(choiceInLower))
+    validatedInput = true;
+    return choiceInLower;
+  }
+}
+
+function checkWinner(playerSelection, computerSelection)
+{
+ if (playerSelection == computerSelection) 
+  {
+    return "Tie";
+  } 
+  else if(
+  (playerSelection == 'rock' && computerSelection == 'scissors') ||
+  (playerSelection == 'scissor' && computerSelection == 'paper') ||
+  (playerSelection == 'paper' && computerSelection == 'rock'))
+  {
+    return 'Player';
+  }
+   else 
+   {
+    return 'Computer';
+   }
+}
+
+
+function playRound(playerSelection,computerSelection)
+{
+  const result = checkWinner(playerSelection,computerSelection);
+  if (result=='tie') {return 'Its a tie'}
+  else if (result =='Player') {return 'You Win'}
+  else 
+  {return 'You lose';}
+}
+
+function game()
+{ let playerScore = 0;
+  let computerScore = 0;
+  console.log('Welcome')
+  for (let i = 0; i < 5; i++)
+
+  {
+    const playerSelection = getPlayerChoice();
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection,computerSelection));
+    if (checkWinner(playerSelection,computerSelection == 'Player'))
+    {
+    playerScore++;
+   }
+   else if (checkWinner(playerSelection,computerSelection == 'Computer'))
+    computerScore++;
+  }
+  console.log('game over')
+  if (playerScore < computerScore)
+    {
+      console.log('Player Won')
+    }
+  else if 
+  (playerScore > computerScore)
+  {
+    console.log('Computer Won')
+  }
+}
+
+game()
